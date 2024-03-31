e9f48cf09061299793ee79abe3e41854719e07d2
SAMBHAV KUMAR MOHANTY <108188982+sambhavkumar0@users.noreply.github.com>
2024-03-31 15:15:40 +0530

    user input added

--- Sorting Algorithms/Bubble Sort/Opt_BS.java
+++ Sorting Algorithms/Bubble Sort/Opt_BS.java
@@ -1,5 +1,5 @@
 /**how is this optimal? here i added a flag variable . if no comparision takes place that means the arr is already sorted so we can break out of the loop prematurely and return the result */
-
+import java.util.Scanner;
 public class Opt_BS {
     public static int[] bubblesort(int[] arr1,int n1){
 
@@ -22,9 +22,17 @@ public class Opt_BS {
         return arr1;
     }
     public static void main(String[] args) {
-        int []arr1 = {2,3,8,6,4,255,64,36,77};
-        int n1=arr1.length;
-        int[] result=bubblesort(arr1, n1);
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the size of the array to be sorted: ");
+        int size =sc.nextInt();
+        int []arr1 =new int[size];
+        System.out.println("Enter the array elements to be sorted ");
+        for (int i=0; i<size; i++) {
+            arr1[i] =sc.nextInt();
+        }
+       
+        int[] result=bubblesort(arr1,size);
         for (int i=0;i<result.length;i++){
             System.out.print(result[i]+" ");
         }
